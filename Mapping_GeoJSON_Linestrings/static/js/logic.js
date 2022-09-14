4b9526b0d576e5cafd156b5802392766415abfdc
Dana Burton <danaburton@Danas-MBP.attlocal.net>
2022-09-13 23:21:35 -0700

    update

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -101,19 +101,41 @@ L.control.layers(baseMaps).addTo(map);
 // let airportData = "https://raw.githubusercontent.com/Danaburtono/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/static/js/majorAirports.json";
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/Danaburtono/Mapping_Earthquakes/main/torontoRoutes.json";
+let torontoData = "https://raw.githubusercontent.com/Danaburtono/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/static/js/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+  color: "yellow",
+  weight: 2
+}
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
+// d3.json(airportData).then(function(data) {
+//     console.log(data);
 
 // Creating a GeoJSON layer with the retrieved data.
- L.geoJSON(data, {
-    onEachFeature: function(feature, layer) {
-      console.log(layer);
-      layer.bindPopup("<h2>" + "Airport Code: " + feature.properties.faa 
-      + "<hr>" + feature.properties.name + "</h2>")
-     }
+//  L.geoJSON(data, {
+//     onEachFeature: function(feature, layer) {
+//       console.log(layer);
+//       layer.bindPopup("<h2>" + "Airport Code: " + feature.properties.faa 
+//       + "<hr>" + feature.properties.name + "</h2>")
+//      }
+
+// }).addTo(map);
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+
+//Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+  // color: "yellow",
+  // weight: 2,
+  style: myStyle,
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup( "<h2>" + "Airline Code: "+ feature.properties.airline + "</h2>" + "<hr>" 
+                 + "<h2>" + "Destination: " + feature.properties.dst + "</h2>")
+  }
 
 }).addTo(map);
 });
